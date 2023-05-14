bca8cbd59aa9188ee50e36b4effb87e718be39c8
Name707 <707VLAN@gmail.com>
2023-05-14 22:06:02 +0600

    Step1

--- app/src/main/java/com/example/lemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp/MainActivity.kt
@@ -6,9 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonadeapp.ui.theme.LemonadeAppTheme
 
@@ -18,30 +26,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+              LemonadeApp()
                 }
             }
         }
     }
-}
 
+
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LemonadeApp() {
+    LemonadeAppActions(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LemonadeAppTheme {
-        Greeting("Android")
+fun LemonadeAppActions(
+    modifier: Modifier = Modifier
+) {
+    var clickResult by remember {
+        mutableStateOf(0)
     }
+
+    val resourceImage = when (clickResult) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        3 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_tree
+    }
+
+    Image (
+        painter = painterResource(resourceImage),
+        contentDescription = clickResult.toString(),
+        modifier = Modifier.clickable { clickResult++ }
+            )
 }
+
